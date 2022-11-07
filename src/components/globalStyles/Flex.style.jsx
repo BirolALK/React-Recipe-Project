40ff1b1fc1c79e943b7fe60f681641faeceb7889
Birol ALKAN <birolalkan93@gmail.com>
2022-11-07 22:52:39 +0300

    button

--- src/components/globalStyles/Flex.style.jsx
+++ src/components/globalStyles/Flex.style.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Flex = styled.div`
     display: flex;
-    justify-content: ${({justifiy}) => (justify ? justify : "center")};
+    justify-content: ${({justifiy}) => (justifiy ? justifiy : "center")};
     align-items : ${({align}) => (align ? align : "center")};
     flex-wrap: ${({wrap}) =>wrap && wrap};
 
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import  {LoginContainer, FormContainer, StyledImg, Header, StyledForm, StyledInput, StyledButton}  from "./Login.style";
+import meal from "../../assets/meal.svg";
+import { useNavigate } from "react-router-dom";
+
+
 
 const Login = () => {
+  
+  const navigate = useNavigate();
+
+  const userInfo={
+    username : "Ahmet"
+
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sessionStorage.setItem("user", JSON.stingify(userInfo))
+    navigate("-1")
+    
+  }
+
   return (
-    <div>Login</div>
+    <LoginContainer>
+      <FormContainer> 
+        <StyledImg src={meal}/>
+        <Header>
+          {"<CW/>"} Recipe
+        </Header>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput placeholer= " Enter username" type="text"/>
+          <StyledInput placeholer= " Enter Passworld" type="password"/>
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
+
+      </FormContainer>
+    </LoginContainer>
   )
 }
 
