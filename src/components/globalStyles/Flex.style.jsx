c9ba92a6c1259f10156d59c02c438ef6446b4e7e
Birol ALKAN <birolalkan93@gmail.com>
2022-11-07 20:00:27 +0300

    styles

--- src/components/globalStyles/Flex.style.jsx
+++ src/components/globalStyles/Flex.style.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Flex = styled.div`
     display: flex;
-    justify-content: ${({justifiy}) => (justify ? justify : "center")}
+    /* justify-content: ${({justifiy}) => (justify ? justify : "center")} */
 `;
 
 export default Flex;
